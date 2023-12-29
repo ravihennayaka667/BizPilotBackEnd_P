3a829a7e58a6d77cd9d856e30f9db299d89a1190
ravi hennayake <ravihennayaka@outlook.com>
2023-12-29 09:21:19 +0530

    Added Dtos And Static Roles

---
+++ BizPilotBackEndProduction/Core/dtos/UpdatePermissonDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BizPilotBackEndProduction.Core.dtos
+{
+    public class UpdatePermissonDto
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+
+    }
+}
