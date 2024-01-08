f7506947d927379c4c298ddc5ea51d438083a5c8
ravi hennayake <ravihennayaka@outlook.com>
2024-01-08 16:10:42 +0530

    db context file added

--- BizPilotBackEndProduction/Core/dbConext/ApplicationDbContext.cs
+++ BizPilotBackEndProduction/Core/dbConext/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BizPilotBackEndProduction.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BizPilotBackEnd.Core.dbContext
@@ -9,8 +10,8 @@ namespace BizPilotBackEnd.Core.dbContext
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
-
         }
 
+        public DbSet<Customers> Customers { get; set; }
     }
 }
