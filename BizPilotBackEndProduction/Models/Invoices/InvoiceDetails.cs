04164f6d818e70af1fc27f4d118cb6007a53b67a
Tharindu Yalegama <sd1@crysbro.net>
2024-03-19 10:16:57 +0530

    updated invoice models

---
+++ BizPilotBackEndProduction/Models/Invoices/InvoiceDetails.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BizPilotBackEndProduction.Models.Invoices
+{
+    public class InvoiceDetails
+    {
+
+
+        [Key]
+        public int InvId { get; set; }
+        public int ItemId { get; set; }
+        public float NoOfUnits { get; set; }
+        public float InvPrice { get; set; }
+        public DateTime SysDate { get; set; }
+
+
+        [ForeignKey("InvId")]
+
+        public virtual ICollection<InvoiceHeader> InvoiceHeaders { get; set; }
+
+    }
+}
---
+++ BizPilotBackEndProduction/Models/Invoices/InvoiceHeader.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BizPilotBackEndProduction.Models.Invoices
+{
+    public class InvoiceHeader
+    {
+
+
+        [Key]
+        public int InvId { get; set; }
+        public string InvCode { get; set; }
+        public DateTime InvDate { get; set; }
+
+        public int CustId { get; set; }
+        public int Discount { get; set; }
+        public float DisAmount { get; set; }
+        public int UserId { get; set; }
+        public float TotalAmount { get; set; }
+        public float GrossInvAmount { get; set; }
+        public bool isCancell { get; set; }
+        public int CancellUser { get; set; }
+
+
+        public virtual ICollection<InvoiceDetails> InvoiceDetails { get; set; }
+    }
+}
